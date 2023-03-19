a614a40976242c96d866736ad03961ad9f447863
Udit <uditde.com@gmail.com>
2023-03-19 14:02:06 +0530

    styles updated & pages added

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+const ChatEngine = dynamic(() =>
+	import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+	import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+const ScrollDownBar = dynamic(() =>
+	import("react-chat-engine").then((module) => module.ScrollDownBar)
+);
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+	const { username, secret, email } = useContext(Context);
+	const [showChat, setShowChat] = useState(false);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (typeof document !== null) {
+			setShowChat(true);
+		}
+	});
+
+	useEffect(() => {
+		if (username.length === 0 || secret.length === 0) {
+			router.push("/");
+		}
+	});
+
+	if (!showChat) return <div />;
+
+	return (
+		<div className="background">
+			<div className="shadow">
+				<ChatEngine
+					height="calc(100vh - 200px)"
+					projectID={`${process.env.NEXT_PUBLIC_PROJECT_ID}`}
+					userName={username}
+					userSecret={secret}
+					userEmail={email}
+					renderNewMessageForm={() => <MessageFormSocial />}
+					renderScrollDownBar={(chat) => <ScrollDownBar chat={chat} />}
+					offset={6}
+				/>
+			</div>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,77 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+	const { username, setUsername, secret, setSecret, email, setEmail } =
+		useContext(Context);
+
+	const router = useRouter();
+
+	function onSubmit(e) {
+		e.preventDefault();
+
+		if (username.length === 0 || secret.length === 0) {
+			console.log(username);
+			console.log(secret);
+			return;
+		}
+
+		//this is how we are creating users
+
+		axios
+			.put(
+				"https://api.chatengine.io/users/",
+				{ username, secret, email },
+				{
+					headers: {
+						"Private-key": `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`,
+					},
+				}
+			)
+			.then((r) => router.push("/chats"));
+	}
+
+	return (
+		<div className="background">
+			<div className="auth-container">
+				<form
+					className="auth-form"
+					autocomplete="off"
+					onSubmit={(e) => onSubmit(e)}
+				>
+					<div className="auth-title">Nxt-Js Cht</div>
+					<div className="input-container">
+						<input
+							autocomplete="false"
+							placeholder="Username"
+							className="text-input"
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+					<div className="input-container">
+						<input
+							autocomplete="false"
+							placeholder="email"
+							className="text-input"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div className="input-container">
+						<input
+							autocomplete="flase"
+							type="password"
+							placeholder="Password"
+							className="text-input"
+							onChange={(e) => setSecret(e.target.value)}
+						/>
+					</div>
+					<button type="submit" className="submit-button">
+						LogIn / Resgister
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
