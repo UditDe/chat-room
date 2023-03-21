4c966babd88bf4a8fdc82231107f3040b88af658
Udit <uditde.com@gmail.com>
2023-03-21 20:37:50 +0530

    minor bug fixed

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../context";
 import { useRouter } from "next/router";
 import axios from "axios";
+import Chats from "./chats";
 
 export default function Auth() {
 	const { username, setUsername, secret, setSecret, email, setEmail } =
@@ -34,44 +35,45 @@ export default function Auth() {
 	}
 
 	return (
-		<div className="background">
-			<div className="auth-container">
-				<form
-					className="auth-form"
-					autocomplete="off"
-					onSubmit={(e) => onSubmit(e)}
-				>
-					<div className="auth-title">Therapist.ai</div>
-					<div className="input-container">
-						<input
-							autocomplete="false"
-							placeholder="Username"
-							className="text-input"
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-					</div>
-					<div className="input-container">
-						<input
-							autocomplete="false"
-							placeholder="email"
-							className="text-input"
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-					<div className="input-container">
-						<input
-							autocomplete="flase"
-							type="password"
-							placeholder="Password"
-							className="text-input"
-							onChange={(e) => setSecret(e.target.value)}
-						/>
-					</div>
-					<button type="submit" className="submit-button">
-						LogIn / Resgister
-					</button>
-				</form>
-			</div>
-		</div>
+		// <div className="background">
+		// 	<div className="auth-container">
+		// 		<form
+		// 			className="auth-form"
+		// 			autocomplete="off"
+		// 			onSubmit={(e) => onSubmit(e)}
+		// 		>
+		// 			<div className="auth-title">Therapist.ai</div>
+		// 			<div className="input-container">
+		// 				<input
+		// 					autocomplete="false"
+		// 					placeholder="Username"
+		// 					className="text-input"
+		// 					onChange={(e) => setUsername(e.target.value)}
+		// 				/>
+		// 			</div>
+		// 			<div className="input-container">
+		// 				<input
+		// 					autocomplete="false"
+		// 					placeholder="email"
+		// 					className="text-input"
+		// 					onChange={(e) => setEmail(e.target.value)}
+		// 				/>
+		// 			</div>
+		// 			<div className="input-container">
+		// 				<input
+		// 					autocomplete="flase"
+		// 					type="password"
+		// 					placeholder="Password"
+		// 					className="text-input"
+		// 					onChange={(e) => setSecret(e.target.value)}
+		// 				/>
+		// 			</div>
+		// 			<button type="submit" className="submit-button">
+		// 				LogIn / Resgister
+		// 			</button>
+		// 		</form>
+		// 	</div>
+		// </div>
+		<Chats />
 	);
 }
